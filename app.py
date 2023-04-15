1745cf358a83ce0d67059800b8b518a8dee74e39
d21it181 <90241914+d21it181@users.noreply.github.com>
2023-04-15 08:45:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+  
+
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world"
+        return jsonify({'data': data})
+        
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8080)
